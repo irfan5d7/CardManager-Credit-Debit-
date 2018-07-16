2495182ada5b5ad9d0235fc9015770c7b0b6f39e
Irfan5d7 <irfanahmed.511.ia@gmail.com>
2018-07-16 11:40:29 +0530

    commit 3

--- CardsWallet/Views/cards.py
+++ CardsWallet/Views/cards.py
@@ -17,61 +17,54 @@ def show_cards(request):
     return render(request,'cards.html',context_dict)
 
 
-
-
-
-
-
-
-
-
-
-class CreateCardView(CreateView):
-    # login_url = '/login/'
-    model = Cards
-    template_name = 'add_card.html'
-    def get_context_data(self, **kwargs):
-        context = super(CreateCardView, self).get_context_data(**kwargs)
-        # profile = UserProfile.objects.filter(user__username=self.request.user.username)
-        # context['prof'] = profile
-        return context
-    def post(self, request, *args, **kwargs):
-        card_form = AddCard(request.POST,request.FILES)
-        if card_form.is_valid():
-            card = card_form.save(commit = False)
-            card.user = self.request.user
-            card.save()
-        return redirect('CardsWallet:index')
-
-class UpdateCardView(UpdateView):
-    # login_url = '/login/'
-    model = Cards
-    form_class = AddCard
-    template_name = 'add_card.html'
-
-    def get_success_url(self):
-        return reverse('CardsWallet:index')
-
-    def get_context_data(self, **kwargs):
-        context = super(UpdateCardView, self).get_context_data(**kwargs)
-        return context
-
-    def post(self, request, *args, **kwargs):
-        card = Cards.objects.get(id=kwargs.get('pk'))
-        form = AddCard(request.POST, instance=card)
-        form.save()
-        return redirect('CardsWallet:index')
-
-
-class DeleteProductView(DeleteView):
-    # login_url = '/login/'
-    model = Cards
-    success_url = reverse_lazy('CardsWallet:index')
-    def get_success_url(self):
-        return reverse('CardsWallet:index')
-    def get(self, request, *args, **kwargs):
-        return self.post(request, args, kwargs)
-    def post(self, request, *args, **kwargs):
-        self.delete(request,args,kwargs)
-        return redirect('CardsWallet:index')
-
+#
+#
+# class CreateCardView(CreateView):
+#     # login_url = '/login/'
+#     model = Cards
+#     template_name = 'add_card.html'
+#     def get_context_data(self, **kwargs):
+#         context = super(CreateCardView, self).get_context_data(**kwargs)
+#         # profile = UserProfile.objects.filter(user__username=self.request.user.username)
+#         # context['prof'] = profile
+#         return context
+#     def post(self, request, *args, **kwargs):
+#         card_form = AddCard(request.POST,request.FILES)
+#         if card_form.is_valid():
+#             card = card_form.save(commit = False)
+#             card.user = self.request.user
+#             card.save()
+#         return redirect('CardsWallet:index')
+#
+# class UpdateCardView(UpdateView):
+#     # login_url = '/login/'
+#     model = Cards
+#     form_class = AddCard
+#     template_name = 'add_card.html'
+#
+#     def get_success_url(self):
+#         return reverse('CardsWallet:index')
+#
+#     def get_context_data(self, **kwargs):
+#         context = super(UpdateCardView, self).get_context_data(**kwargs)
+#         return context
+#
+#     def post(self, request, *args, **kwargs):
+#         card = Cards.objects.get(id=kwargs.get('pk'))
+#         form = AddCard(request.POST, instance=card)
+#         form.save()
+#         return redirect('CardsWallet:index')
+#
+#
+# class DeleteProductView(DeleteView):
+#     # login_url = '/login/'
+#     model = Cards
+#     success_url = reverse_lazy('CardsWallet:index')
+#     def get_success_url(self):
+#         return reverse('CardsWallet:index')
+#     def get(self, request, *args, **kwargs):
+#         return self.post(request, args, kwargs)
+#     def post(self, request, *args, **kwargs):
+#         self.delete(request,args,kwargs)
+#         return redirect('CardsWallet:index')
+#
--- CardsWallet/urls.py
+++ CardsWallet/urls.py
@@ -2,11 +2,14 @@ from django.conf.urls import url
 from django.urls import path
 
 from CardsWallet.Views import *
-from CardsWallet.Views.login import SignUpController
+from CardsWallet.Views.login import SignUpController, LoginController, logout_user
 
 app_name = 'CardsWallet'
 urlpatterns =[
     url('cards/',show_cards,name='cards_views'),
     url(r'^signup/$',SignUpController.as_view(),name = 'signup'),
+    path('login/',LoginController.as_view(),name='login'),
+    path('logout/',logout_user,name = 'logout'),
+    # url('add_cards/',CreateCardView,name='add_card'),
 
     ]
